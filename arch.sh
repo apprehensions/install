6a76248b65fcfb761e442d87feb2b7463f892702
wael <47404953+wwwael@users.noreply.github.com>
2021-05-05 19:49:09 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -24,7 +24,7 @@ echo "::1       localhost" >> /mnt/etc/hosts
 echo "127.0.1.1 $name.localdomain $name" >> /mnt/etc/hosts
 echo "$name" > /mnt/etc/hostname
 
-arch-chroot /mnt useradd -m -G wheel -s /bin/bash wael
+arch-chroot /mnt useradd -m -s /bin/bash wael
 echo "wael ALL=(ALL) ALL" >> /mnt/etc/sudoers.d/wael
 arch-chroot /mnt passwd wael
 arch-chroot /mnt passwd
