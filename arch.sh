d03d9a5520e228cd897b68b09d9697b2fa341189
Your Name <40663@protonmail.com>
2021-05-19 15:13:37 +0300

    ok

--- arch.sh
+++ arch.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
-name="arch"
-read -p "change name different than arch? [y/n]" answer
-if [[ $answer = y ]] ; then
-	echo "name: "
-	read name
-fi
+echo "root: "
+read ROOT
+echo "esp: "
+read ESP
+cryptsetup luksFormat $ROOT
+cryptsetup open $ROOT croot
+mkfs.ext4 /dev/mapper/croot
+mount /dev/mapper/croot /mnt
+mkdir /mnt/boot
+mount $ESP /mnt/boot
 
-pacstrap /mnt linux linux-firmware linux-headers base base-devel btrfs-progs
+pacstrap /mnt linux linux-firmware linux-headers base intel-ucode networkmanager intel-media-driver base-devel git sof-firmware acpid alsa-ucm-conf alsa-utils mesa xf86-video-intel
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# bootloader
+arch-chroot /mnt bootctl install
+UUID = $(blkid -s UUID -o value $ROOT)
+echo "options rw cryptdevice=UUID=$UUID:croot root=/dev/mapper/croot" >> /mnt/boot/loader/entries/arch.conf
+arch-chroot locale-gen
+# time
 ln -sf /mnt/usr/share/zoneinfo/Asia/Riyadh /mnt/etc/localtime
 arch-chroot /mnt timedatectl set-ntp true && hwclock --systohc
-
-echo '"boot" "rw root=$part rootflags=subvol=@"' >> /mnt/boot/refind_linux.conf
-echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
-arch-chroot /mnt locale-gen
-
-echo "KEYMAP=us" >> /mnt/etc/vconsole.conf
-echo "127.0.0.1 localhost" > /mnt/etc/hosts
-echo "::1       localhost" >> /mnt/etc/hosts
-echo "127.0.1.1 $name.localdomain $name" >> /mnt/etc/hosts
-echo "$name" > /mnt/etc/hostname
-
+# creating me :3
 arch-chroot /mnt useradd -m -s /bin/bash wael
-echo "wael ALL=(ALL) ALL" >> /mnt/etc/sudoers.d/wael
-arch-chroot /mnt passwd wael
-arch-chroot /mnt passwd
 
-# i use arch-chroot due to complications
-arch-chroot /mnt pacman -Syu xdg-user-dirs xdg-user-dirs networkmanager nvidia nvidia-utils nvidia-settings xorg xorg-xinit xclip pulseaudio pulseaudio-alsa
-arch-chroot /mnt systemctl enable NetworkManager
 
