461c47ff8c887d9c3d29478cb63252f92ddac600
wael <47404953+wael444@users.noreply.github.com>
2021-06-09 09:29:40 +0300

    hdfsgjsrtyj4tyrjrysjry

--- arch.sh
+++ arch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# these are literally made for me, please never use this unless you are wael
 
 echo "root > "
 read root
@@ -35,19 +36,19 @@ fi
 
 mkdir /mnt/boot
 mount $esp /mnt/boot
-pacstrap /mnt linux linux-firmware linux-headers base base-devel intel-ucode btrfs-progs sof-firmware xf86-video-intel mesa
+pacstrap /mnt linux linux-firmware linux-headers base base-devel intel-ucode btrfs-progs sof-firmware xf86-video-intel mesa zsh
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt bootctl install
-cp -v -r root/etc/sudoers.d /mnt/etc/
 cp -v -r root/etc/systemd /mnt/etc/
-echo "LANG=en_us.UTF-8" > /etc/locale.conf
-echo "en_us.UTF-8 UTF-8" > /etc/locale.gen
 cp -v -r root/boot /mnt/
 echo $name > /mnt/etc/hostname
 echo > /mnt/etc/issue
 echo "127.0.0.1 localhost" > /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 $name.localdomain $name" >> /etc/hosts
+echo "wael ALL=(ALL) ALL" > /etc/sudoers.d/wael
+echo "LANG=en_us.UTF-8" > /etc/locale.conf
+echo "en_us.UTF-8 UTF-8" > /etc/locale.gen
 
 if [[ $HOST = laptop ]] ; then
 	echo "options rw cryptdevice=$root:croot:allow-discards root=/dev/mapper/croot rootflags=subvol=@" >> /mnt/boot/loader/entries/arch.conf
@@ -57,9 +58,9 @@ fi
 arch-chroot /mnt mkinitcpio -P linux
 arch-chroot /mnt timedatectl set-ntp true
 arch-chroot /mnt timedatectl set-timezone Asia/Riyadh
-arch-chroot /mnt useradd -m -s /bin/bash wael
+arch-chroot /mnt useradd -m -s /bin/zsh wael
 arch-chroot /mnt locale-gen
-passwd 
-passwd wael
+arch-chroot /mnt passwd wael
+arch-chroot /mnt passwd
 
 # https://github.com/Bugswriter/arch-linux-magic/blob/master/arch_install.sh
