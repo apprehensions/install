6725ef2e6b30185a77bd204f79b199c83c6ec910
wael444 <40663@protonmail.com>
2021-06-27 22:38:56 +0000

    im shit at sed

--- arch.sh
+++ arch.sh
@@ -26,11 +26,14 @@ echo -e $name > /etc/hostname
 echo -e "127.0.0.1 localhost\n::1       localhost \n127.0.0.1 $name.localdomain $name" > /mnt/etc/hosts
 echo -e "[Match]\nName=eno1\n\n[Network]\nDHCP=yes" > /mnt/etc/systemd/network/lan.network
 mkdir /mnt/etc/systemd/system/getty@tty1.service.d
-echo -e "[Service]\nExecStart=\nExecstart=-/usr/bin/agetty --skip-login --no-newline --noissue --autologin wael\nTTYVTDisallocate=no" > /mnt/etc/systemd/system/getty@tty1.service.d/override.conf
+echo -e "[Service]\nExecStart=\nExecstart=-/usr/bin/agetty --skip-login --nonewline --noissue --autologin wael\nTTYVTDisallocate=no" > /mnt/etc/systemd/system/getty@tty1.service.d/override.conf
+arch-chroot /mnt bootctl install
+echo -e "title   Arch Linux\nlinux   /vmlinuz-linux\ninitrd  /intel-ucode.img\ninitrd  /initramfs-linux.img\noptions rw root=$root rootflags=subvol=@ loglevel=4" > /boot/loader/entries/arch.conf
+echo -e "timeout 1\nconsole-mode max" > /boot/loader/loader.conf
 echo -e "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 sed -i '177s/.//' /mnt/etc/locale.gen
 sed -i '82s/. //' /mnt/etc/sudoers
-sed -i '33s/.//' /mnt/etc/pacman.conf && sed -i '36s/.//' /mnt/etc/pacman.conf && sed -i '37s/.//' /mnt/etc/pacman.conf && sed -i '37s/5/12/' /mnt/etc/pacman.conf
+sed -i '33s/.//' /mnt/etc/pacman.conf && sed -i '36s/.//' /mnt/etc/pacman.conf && sed -i '37s/.//' /mnt/etc/pacman.conf && sed -i '37s/5/12/' /mnt/etc/pacman.conf && sed -i '93/.//' /mnt/etc/pacman.conf && sed -i '94/.//' /mnt/etc/pacman.conf
 sed '1,/^#part2$/d' arch.sh > /mnt/part2.sh
 chmod +x /mnt/part2.sh
 arch-chroot /mnt ./part2.sh
@@ -39,8 +42,6 @@ exit
 #part2
 locale-gen
 bootctl install
-echo -e "title   Arch Linux\nlinux   /vmlinuz-linux\ninitrd  /intel-ucode.img\ninitrd  /initramfs-linux.img\noptions rw root=$root rootflags=subvol=@ loglevel=4" > /boot/loader/entries/arch.conf
-echo -e "timeout 1\nconsole-mode max" > /boot/loader/loader.conf
 timedatectl set-ntp true
 timedatectl set-timezone Asia/Riyadh
 pacman -S git wget zsh nvidia-settings neofetch openssh reflector 
