5950ed5d088b27f89aeda0210c2c17314aa55629
wael444 <40663@protonmail.com>
2021-07-04 02:28:18 +0300

    fu

--- wael_laptop.sh
+++ wael_laptop.sh
@@ -3,17 +3,15 @@
 # https://gist.github.com/gbrlsnchs/9c9dc55cd0beb26e141ee3ea59f26e21
 # variables
 BTRFS_OPTS="rw,relatime,ssd,compress=zstd,space_cache,commit=120"
-REPO=https://alpha.de.repo.voidlinux.org/
+REPO=https://alpha.de.repo.voidlinux.org/current
 XBPS_ARCH=x86_64
 HOSTNAME=yoga
-ROOT="/dev/nvme0n1p1"
-BOOT="/dev/nvme0n1p2"
-BOOT_UUID=$(blkid -s UUID -o value $ROOT)
-ROOT_UUID=$(blkid -s UUID -o value $BOOT)
+ROOT="/dev/nvme0n1p2"
+BOOT="/dev/nvme0n1p1"
 
 mkfs.vfat -nGRUB -F32 $ESP
 cryptsetup -y -v luksFormat -s=512 $ROOT
-cryptsetup open $root kroot
+cryptsetup open $ROOT kroot
 mkfs.btrfs -L void -f /dev/mapper/kroot
 mount -o $BTRFS_OPTS /dev/mapper/kroot /mnt
 
@@ -36,8 +34,8 @@ btrfs subvolume create /mnt/var/cache/xbps
 btrfs subvolume create /mnt/var/tmp
 btrfs subvolume create /mnt/srv
 
-for dir in dev proc sys run; do mount --rbind /$dir /mnt/$dir; mount --make-rslave /mnt/$dir; done
 xbps-install -Sy -R $REPO -r /mnt base-system base-devel btrfs-progs cryptsetup grub-x86_64-efi iwd
+for dir in dev proc sys run; do mount --rbind /$dir /mnt/$dir; mount --make-rslave /mnt/$dir; done
 
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/default/libc-locales
@@ -47,7 +45,9 @@ echo 'tmpdir=/tmp' >> /mnt/etc/dracut.conf
 echo -e "127.0.0.1 localhost\n::1       localhost \n127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" > /mnt/etc/hosts
 cp -P /etc/resolv.conf /mnt/etc/
 
-cat <<EOF > /etc/fstab
+ROOT_UUID=$(blkid -s UUID -o value $ROOT)
+BOOT_UUID=$(blkid -s UUID -o value $BOOT)
+cat <<EOF > /mnt/etc/fstab
 UUID=$ROOT_UUID /       btrfs $BTRFS_OPTS,subvol=@ 0 1
 UUID=$BOOT_UUID /boot   vfat  defaults,noatime 0 2
 UUID=$ROOT_UUID /home   btrfs $BTRFS_OPTS,subvol=@home 0 2
@@ -73,8 +73,11 @@ GRUB_CMDLINE_LINUX="cryptdevice=UUID=$ROOT_UUID:kroot:allow-discards"
 GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4 rd.auto=1"
 EOF
 chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-chroot /mnt ln -s /etc/sv/acpid /mnt/var/service/
-chroot /mnt ln -s /etc/sv/iwd /mnt/var/service/
+ln -s /mnt/etc/sv/acpid /mnt/var/service/
+ln -s /mnt/etc/sv/iwd /mnt/var/service/
+ln -s /mnt/etc/sv/dbus /mnt/var/service
 chroot /mnt xbps-reconfigure -f glibc-locales
+chroot /mnt xbps-install -Su void-repo-nonfree void-repo-multilib
+chroot /mnt xbps-install -Sy intel-ucode
 chroot /mnt useradd -m -G wheel,input,video -s /bin/zsh wael
 chroot /mnt passwd && passwd wael
