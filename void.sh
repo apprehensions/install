9bdb72e3f84defa181ae24bd078d929c56d9329e
wael444 <40663@protonmail.com>
2021-07-07 20:45:31 +0000

    sed

--- void.sh
+++ void.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # https://gist.github.com/tobi-wan-kenobi/bff3af81eac27e210e1dc88ba660596e
 # https://gist.github.com/gbrlsnchs/9c9dc55cd0beb26e141ee3ea59f26e21
+set -x
 BTRFS_OPTS="rw,relatime,ssd,compress=zstd,space_cache,commit=120"
 REPO="https://alpha.de.repo.voidlinux.org/current"
 ARCH=x86_64
@@ -14,12 +15,12 @@ mount -o $BTRFS_OPTS $ROOT /mnt
 mkdir /mnt/efi
 mount -o rw,noatime $ESP /mnt/efi
 
-XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R $REPO base-system base-devel btrfs-progs grub-x86_64-efi elogind
+XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R $REPO base-system base-devel btrfs-progs grub-x86_64-efi
 for dir in dev proc sys run; do mount --rbind /$dir /mnt/$dir; mount --make-rslave /mnt/$dir; done
 cp /etc/resolv.conf /mnt/etc/
 
 chroot /mnt xbps-install -Syu void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
-chroot /mnt xbps-install -Sy intel-ucode nvidia zsh zsh-syntax-highlighting 
+chroot /mnt xbps-install -Sy intel-ucode nvidia zsh zsh-syntax-highlighting elogind
 
 echo $HOSTNAME > /mnt/etc/hostname
 cat <<EOF > /mnt/etc/rc.conf
@@ -31,9 +32,11 @@ EOF
 
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/default/libc-locales
+
 sed -i "/GETTY_ARGS=/s/\"$/ --autologin wael&/" /mnt/etc/sv/agetty-tty1/conf
+sed -i '82s/.//' /mnt/etc/sudoers
 chroot /mnt useradd -m -G wheel,input,video,kvm,storage -s /bin/zsh wael
-chroot /mnt passwd wael
+chroot /mnt passwd 
 
 ROOT_UUID=$(blkid -s UUID -o value $ROOT)
 ESP_UUID=$(blkid -s UUID -o value $ESP)
@@ -43,11 +46,14 @@ UUID=$ESP_UUID /efi   vfat  defaults,noatime 0 2
 tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0
 EOF
 
+sed -i -e '15s/.//' -e '9s/4/5/' /mnt/etc/default/grub
 chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id="Void Linux"
 chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
 chroot /mnt ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
 chroot /mnt ln -s /etc/sv/dhcpcd /etc/runit/runsvdir/default/
 chroot /mnt ln -s /etc/sv/elogind /etc/runit/runsvdir/default/
-chroot /mnt passwd
+
+sleep 5
 chroot /mnt xbps-reconfigure -fa
 umount -R /mnt
