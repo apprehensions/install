9ef73a5c4926b8ba2f8f3091cf7eb52a4fe84806
wael444 <40663@protonmail.com>
2021-07-10 00:54:53 +0300

    voider

--- void-lp.sh
+++ void-lp.sh
@@ -1,27 +1,32 @@
->#!/bin/bash
+#!/bin/bash
 BTRFS_OPTS="rw,relatime,ssd,compress=zstd,space_cache,commit=120"
 REPO=https://alpha.de.repo.voidlinux.org/current
-ARCH=x86_64
-HOSTNAME=br
-ROOT="/dev/sda2"
-ESP="/dev/sda1"
+XBPS_ARCH=x86_64
+HOSTNAME=yoga
+ROOT="/dev/nvme0n1p2"
+ESP="/dev/nvme0n1p1"
 
 mkfs.vfat -nGRUB -F32 $ESP
-mkfs.btrfs -L void -f $ROOT
-mount -o $BTRFS_OPTS $ROOT /mnt
-mkdir /mnt/efi
-mount -o rw,noatime $ESP /mnt/efi
+cryptsetup -v luksFormat -s=512 $ROOT
+cryptsetup open $ROOT kroot
+mkfs.btrfs -L void -f /dev/mapper/kroot
+mount -o $BTRFS_OPTS /dev/mapper/kroot /mnt
+mkdir -p /mnt/boot
+mount -o rw,noatime $ESP /mnt/boot
 
-XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R $REPO base-system base-devel btrfs-progs grub-x86_64-efi elogind dbus socklog-unix
+XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R $REPO base-system base-devel btrfs-progs cryptsetup grub-x86_64-efi iwd elogind dbus socklog-unix
 for dir in dev proc sys run; do mount --rbind /$dir /mnt/$dir; mount --make-rslave /mnt/$dir; done
 cp /etc/resolv.conf /mnt/etc/
 
 chroot /mnt xbps-install -Syu void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
-chroot /mnt xbps-install -Sy intel-ucode nvidia zsh zsh-syntax-highlighting
+chroot /mnt xbps-install -Sy intel-ucode mesa-dri zsh zsh-syntax-highlighting
 
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/default/libc-locales
-echo $HOSTNAME > /mnt/etc/hostname
+echo 'add_dracutmodules+="crypt btrfs"' >> /mnt/etc/dracut.conf
+echo hostonly=yes >> /mnt/etc/dracut.conf
+echo 'tmpdir=/tmp' >> /mnt/etc/dracut.conf
+echo $HOSTNAME >  /mnt/etc/hostname
 cat <<EOF > /mnt/etc/rc.conf
 HOSTNAME="$HOSTNAME"
 HARDWARECLOCK="UTC"
@@ -33,14 +38,16 @@ ROOT_UUID=$(blkid -s UUID -o value $ROOT)
 ESP_UUID=$(blkid -s UUID -o value $ESP)
 cat <<EOF > /mnt/etc/fstab
 UUID=$ROOT_UUID /       btrfs $BTRFS_OPTS 0 1
-UUID=$ESP_UUID /efi   vfat  defaults,noatime 0 2
+UUID=$ESP_UUID /boot   vfat  defaults,noatime 0 2
 tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0
 EOF
 
-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id="Void Linux"
-sed -i -e '15s/.//' -e '9s/4/5/' /mnt/etc/default/grub
+chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Void Linux"
+sed -i "/GRUB_CMDLINE_LINUX=/s/\"$/ cryptdevice=UUID=$ROOT_UUID:kroot rd.luks.allow-discards&/" /mnt/etc/default/grub
+sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/s/\"$/ rd.auto=1 slub_debug=P page_poison=1 loglevel=5 intel_iommu=igfx_off&/" /mnt/etc/default/grub
 chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
+chroot /mnt ln -sv /etc/sv/iwd /etc/runit/runsvdir/default
 chroot /mnt ln -sv /etc/sv/dbus /etc/runit/runsvdir/default
 chroot /mnt ln -sv /etc/sv/dhcpcd /etc/runit/runsvdir/default
 chroot /mnt ln -sv /etc/sv/nanoklogd /etc/runit/runsvdir/default
