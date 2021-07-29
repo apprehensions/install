e1ece9811c28e87f5bfca47ca94fc7b9267e91bb
wael444 <40663@protonmail.com>
2021-07-29 15:29:28 +0000

    ok

--- art-lp.sh
+++ art-lp.sh
@@ -2,21 +2,17 @@
 BTRFS_OPTS="rw,relatime,ssd,compress=zstd,space_cache,commit=120"
 export ROOT="/dev/nvme0n1p2"
 ESP="/dev/nvme0n1p1"
-export HOST=lp_artix
+export HOST=lp_art
 export HOSTNAME=yoga
 
-read PASS
-
-mkfs.vfat -nESP -F32 $ESP
-echo -n $PASS | cryptsetup -v luksFormat -s=512 $ROOT -d -
-echo -n $PASS | cryptsetup open $ROOT kroot -d -
-mkfs.btrfs -L kroot /dev/mapper/kroot
-mount -o $BTRFS_OPTS /dev/mapper/kroot /mnt
+mkfs.vfat -nBL -F32 $ESP
+mkfs.btrfs -L root -f $ROOT
+mount -o $BTRFS_OPTS $ROOT /mnt
 mkdir /mnt/boot
 mount -o rw,noatime $ESP /mnt/boot
 
 sed -ibak -e '37s/.//' -e '37s/5/20/' /etc/pacman.conf
-basestrap /mnt base base-devel linux linux-firmware linux-headers intel-ucode btrfs-progs openrc elogind-openrc iwd-openrc dhcpcd-openrc artix-archlinux-support grub os-prober efibootmgr cryptsetup
+basestrap /mnt base base-devel linux linux-firmware linux-headers intel-ucode btrfs-progs openrc elogind-openrc iwd-openrc dhcpcd-openrc artix-archlinux-support grub os-prober efibootmgr
 mv /etc/pacman.confbak /etc/pacman.conf
 fstabgen -U /mnt >> /mnt/etc/fstab
 
@@ -25,7 +21,6 @@ cp ./modules /mnt/ -r
 chmod a+x /mnt/post.sh
 artix-chroot /mnt ./post.sh
 umount -R /mnt
-cryptsetup close kroot
 exit
 
 # - post
--- modules/40-boot.sh
+++ modules/40-boot.sh
@@ -2,13 +2,12 @@ uuid_root=$(blkid -o value -s UUID $ROOT)
 
 if [[ "$HOST" = lp_art ]]; then
 	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id='Artix Linux'
-	sed -i '13s/.//' /etc/default/grub
-	sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/s/\"$/ slub_debug=P nowatchdog page_poison=1 loglevel=5 intel_iommu=igfx_off&/" /etc/default/grub
-	sed -i "/GRUB_CMDLINE_LINUX=/s/\"$/cryptdevice=UUID=$uuid_root:kroot&/" /etc/default/grub
+	sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/s/\"$/ slub_debug=P nowatchdog page_poison=1 loglevel=5&/" /etc/default/grub
 fi
 
 if [[ "$HOST" = pc_art ]]; then
 	grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id='Artix Linux'
 fi
 
+echo -e "\nGRUB_DISABLE_OS_PROBER=false" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
