b132622e410733f436cae8f9dc61e58a89d9d27f
wael <40663@proton.me>
2022-07-17 12:47:37 +0300

    ok

--- disk.sh
+++ disk.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env -S bash -xe
-_disk=/dev/sda
-sgdisk -Z ${_disk}
-sgdisk -a 2048 -o ${_disk}
-sgdisk -n 1:0:+512M ${_disk} # /dev/sda1
-sgdisk -n 2:0:0 ${_disk} # /dev/sda2
-mkfs.vfat -F 32 -n ESP ${_disk}1
-mkfs.btrfs -L EPHEMERA -f ${_disk}2
-sgdisk -t 1:ef00 ${_disk} 
-sgdisk -t 2:8300 ${_disk} 
-mount -o rw,noatime,ssd,space_cache=v2,discard=async ${_disk}2 /mnt
+#!/usr/bin/env -S sh -xe
+. source.conf
+sgdisk -Z ${DISK}
+sgdisk -a 2048 -o ${DISK}
+sgdisk -n 1:0:+512M ${DISK} # /dev/sda1
+sgdisk -n 2:0:0 ${DISK} # /dev/sda2
+mkfs.vfat -F 32 -n ${ESPLABEL} ${DISK}1
+mkfs.btrfs -L ${ROOTLABEL} -f ${DISK}2
+sgdisk -t 1:ef00 ${DISK} 
+sgdisk -t 2:8300 ${DISK} 
+mount -o ${BTRFSOPTS} ${DISK}2 /mnt
 mkdir -pv /mnt/boot
-mount -o noatime ${_disk}1 /mnt/boot
+mount -o noatime ${DISK}1 /mnt/boot
--- void.sh
+++ void.sh
@@ -1,47 +1,56 @@
 #!/usr/bin/env -S bash -xe
+. source.conf
+
 export REPO=https://mirror.fit.cvut.cz/voidlinux
-export ARCH=x86_64
-XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R $REPO/current \
-  base-minimal base-devel linux-mainline{,-headers} kbd intel-ucode \
-  btrfs-progs dosfstools gummiboot pciutils usbutils iproute2 \
-  socklog-void xtools git ncurses file man{,-pages} \
-  dumb_runtime_dir dhcpcd opendoas acpid nvidia dbus \
-  exa grc android-udev-rules android-tools bluez
+XBPS_ARCH=x86_64 xbps-install -S -r /mnt -R $REPO/current ${PKGS}
 
-## neccessary mounting for literally anything
 mount -t efivarfs efivarfs /sys/firmware/efi/efivars
-for mount in sys dev proc; do
-  mount --rbind /$mount /mnt/$mount
-done
+for mount in sys dev proc; do mount --rbind /$mount /mnt/$mount; done
 
-# good luck trying to understand this
-chmod +x genfstab
-cp -v genfstab /mnt/usr/bin/
-./genfstab -U /mnt > /mnt/etc/fstab
-useradd -R /mnt -mG audio,video,input,kvm,socklog,plugdev,adbusers,bluetooth -s /bin/zsh wael
+useradd -R /mnt -mG audio,video,input,kvm,socklog,plugdev,adbusers,bluetooth wael
 echo "wael:meow" | chpasswd -R /mnt -c SHA512
 echo "root:meow" | chpasswd -R /mnt -c SHA512
-echo ephemera > /mnt/etc/hostname
-chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 sed -i "/GETTY_ARGS=/s/\"$/ --autologin wael&/" /mnt/etc/sv/agetty-tty1/conf
-printf '%s\n%s\n' 'permit persist wael' 'permit nopass wael cmd xbps-install' > /mnt/etc/doas.conf
 sed -i '/^.*pam_dumb_runtime.*/s/.//' /mnt/etc/pam.d/system-login
+echo ephemera >/mnt/etc/hostname
+chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 sed -i '/^#en_US.UTF-8/s/.//' /mnt/etc/default/libc-locales
-printf '%s\n' nameserver\ {9.9.9.9,149.112.112.112} > /mnt/etc/resolv.conf
-printf "%s\n" {hostonly{,_cmdline},show_modules}=yes > /mnt/etc/dracut.conf.d/options.conf
-echo 'compress="cat"' >> /mnt/etc/dracut.conf.d/options.conf
-printf '%s\n%s\n' 'blacklist nouveau' options\ nvidia{-drm\ modeset,\ NVreg_UsePageAttributeTable}=1 > /mnt/etc/modprobe.d/nvidia.conf
 mkdir -pv /mnt/etc/sysctl.d
 echo "kernel.dmesg_restrict=0" > /mnt/etc/sysctl.d/99-dmesg-user.conf
-chroot /mnt gummiboot install
-echo "rw mitigations=off nowatchdog" > /mnt/boot/loader/void-options.conf
+
+cat <<EOFSTAB > /mnt/etc/fstab
+LABEL=${ROOTLABEL} / btrfs ${BTRFSOPTS} 0 0
+LABEL=${ESPLABEL} /boot vfat defaults 0 2
+EOFSTAB
+cat <<EODOASCONF > /mnt/etc/doas.conf
+permit persist wael
+permit nopass wael cmd xbps-install
+EODOASCONF
+cat <<EOMODPROBENVIDIACONF > /mnt/etc/modprobe.d/nvidia.conf
+blacklist nouveau
+options nvidia-drm modeset=1
+options nvidia NVreg_UsePageAttributeTable=1
+EOMODPROBENVIDIACONF
+cat <<EODRACUTCONF > /mnt/etc/dracut.conf.d/options.conf
+hostonly=yes
+hostonly_cmdline=yes
+show_modules=yes
+compress="cat"
+EODRACUTCONF
+cat <<EOEFISTUB > /mnt/etc/default/efibootmgr-kernel-hook
+MODIFY_EFI_ENTRIES=1
+OPTIONS="rw mitigations=off loglevel=6 nowatchdog"
+DISK=$DISK
+PART=1
+EOEFISTUB
+
 mkdir -p /mnt/etc/xbps.d
-xbps-install -Sy -r /mnt -R $REPO/current void-repo-{multilib{,-nonfree},nonfree}
+xbps-install -Sy -r /mnt -R $REPO/current \
+	void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
 cp /mnt/usr/share/xbps.d/*-repository-*.conf /mnt/etc/xbps.d/
 sed -i "s|https://repo-default.voidlinux.org|$REPO|g" /mnt/etc/xbps.d/*-repository-*.conf
-printf "%s\n" ignorepkg=linux{,-headers,-firmware-{amd,broadcom}} > /mnt/etc/xbps.d/99-ignore.conf
-xbps-remove -y -r /mnt linux{,-headers,-firmware-{amd,broadcom}}
-for sv in acpid dhcpcd socklog-unix nanoklogd dbus bluetoothd; do
-  chroot /mnt ln -sfv /etc/sv/$sv /etc/runit/runsvdir/default
+for service in acpid dhcpcd socklog-unix nanoklogd dbus bluetoothd; do
+  chroot /mnt ln -sfv /etc/sv/$service /etc/runit/runsvdir/default
 done
+xbps-install -r /mnt -Syuv intel-ucode nvidia
 xbps-reconfigure -r /mnt -fa
